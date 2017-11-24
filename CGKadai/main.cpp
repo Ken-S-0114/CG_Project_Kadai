47010ddf7efee2d85ece4f7713cf6bb0a4ea41b4
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2017-11-25 00:11:24 +0900

    Initial Commit

---
+++ CGKadai/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CGKadai
+//
+//  Created by 佐藤賢 on 2017/11/25.
+//  Copyright © 2017年 佐藤賢. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
